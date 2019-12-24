12e88f6ca7f347478f682943f700ff9d572594d3
Austin Songer <austinvernsonger@protonmail.com>
2019-12-24 16:59:52 -0600

    Create upgrade.sh

---
+++ upgrade.sh
@@ -0,0 +1,60 @@
+systemctl stop filebeat
+systemctl stop kibana
+
+curl -s https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee /etc/apt/sources.list.d/elastic-7.x.list
+
+
+curl -X PUT "localhost:9200/_cluster/settings" -H 'Content-Type: application/json' -d'
+{
+  "persistent": {
+    "cluster.routing.allocation.enable": "primaries"
+  }
+}
+'
+
+curl -X POST "localhost:9200/_flush/synced"
+
+
+systemctl stop elasticsearch
+
+apt-get install elasticsearch=7.5.1
+systemctl restart elasticsearch
+systemctl daemon-reload
+systemctl restart elasticsearch
+
+curl -X GET "localhost:9200/_cat/nodes"
+
+
+curl -X PUT "localhost:9200/_cluster/settings" -H 'Content-Type: application/json' -d'
+{
+  "persistent": {
+    "cluster.routing.allocation.enable": null
+  }
+}
+'
+
+curl -X GET "localhost:9200/_cat/health?v"
+
+
+
+apt-get install filebeat=7.5.1
+cp /etc/filebeat/filebeat.yml /backup/filebeat.yml.backup
+curl -so /etc/filebeat/filebeat.yml https://raw.githubusercontent.com/wazuh/wazuh/v3.11.0/extensions/filebeat/7.x/filebeat.yml
+chmod go+r /etc/filebeat/filebeat.yml
+curl -s https://packages.wazuh.com/3.x/filebeat/wazuh-filebeat-0.1.tar.gz | sudo tar -xvz -C /usr/share/filebeat/module
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
